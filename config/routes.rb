1cc85d96dbb188ed26a2bef595476fd8c67ceb25
Masyhuri Farhan <72302847+masy-web@users.noreply.github.com>
2022-04-16 11:36:21 +0000

    Add menus mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'menus/index'
+  get 'menus/show'
+  get 'menus/new'
+  get 'menus/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
